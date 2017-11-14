80d6b03cb2bb46fdfc6dc23474e6432a5084db88
jonasfilipetrindadeantunes <33063075+jonasfilipetrindadeantunes@users.noreply.github.com>
2017-11-14 21:00:47 +0000

    Update main.cpp

--- aeda/main.cpp
+++ aeda/main.cpp
@@ -7,9 +7,28 @@ Companhia c;
 
 void menu();
 
+void avioes()
+{
+	cout<<"------AVIOES-------"<<endl;
+	cout<<"Ver todos os avioes(1)"<<endl;
+	cout<<"MENU(5)"<<endl;
+
+	int numero;
+	cin>>numero;
+
+	switch(numero)
+	{
+		case 1:
+			c.vertodosAvioes();
+			avioes();
+			break;
+		case 5:
+			menu();
+	}
+}
 void reservas()
 {
-	cout<<"------PASSAGEIROS-------"<<endl;
+	cout<<"------RESERVAS-------"<<endl;
 	cout<<"Ver todas as reservas(1)"<<endl;
 	cout<<"MENU(5)"<<endl;
 	
@@ -70,6 +89,7 @@ void menu()
 	cout<<"--------COMPANHIA Reserva de Voos-----"<<endl;
 	cout<<"Passageiros(1)"<<endl;
 	cout<<"Reservas(2)"<<endl;
+	cout<<"Avioes(3)"<<endl;
 	cout<<"Sair do menu(4)"<<endl;
 
 	int numero;
@@ -84,6 +104,9 @@ void menu()
 	case 2:
 		reservas();
 		break;
+	case 3:
+		avioes();
+		break;
 	case 4:
 		exit(1);
 	}
@@ -96,11 +119,11 @@ int main()
 	
 	c.carregarPassageiros();
 	c.carregarReservas();
+	c.carregarAvioes();
 
 	menu();
 	
 	return 0;
 
 }
-
 
